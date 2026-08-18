4d244ba433290e070ce217241dda85bf79c69308
martelode38 <marteniers@>
2026-08-18 16:31:59 -0300

    Initial Commit

---
+++ SocketProject/SocketProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocketProjectApp.swift
+//  SocketProject
+//
+//  Created by Martenier Santos on 18/08/26.
+//
+
+import SwiftUI
+
+@main
+struct SocketProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
